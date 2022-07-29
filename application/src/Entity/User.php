a9557b8630dfddace2fe1d44fde0c8ce650eb037
Olivia De Scheerder <olivia@les-tilleuls.coop>
2022-07-29 14:03:47 +0200

    update: persisting users with roles

--- application/src/Entity/User.php
+++ application/src/Entity/User.php
@@ -20,31 +20,31 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    private int $id;
 
     /**
      * @ORM\Column(type="string", length=25, unique=true)
      * @Assert\NotBlank(message="Vous devez saisir un nom d'utilisateur.")
      */
-    private $username;
+    private string $username;
 
     /**
      * @ORM\Column(type="string", length=64)
      */
-    private $password;
+    private string $password;
 
     /**
-     * @ORM\Column(type="string", length=64)
+     * @ORM\Column(type="json", length=64)
      * @var array
      */
-    private $roles = [];
+    private array $roles = [];
 
     /**
      * @ORM\Column(type="string", length=60, unique=true)
      * @Assert\NotBlank(message="Vous devez saisir une adresse email.")
      * @Assert\Email(message="Le format de l'adresse n'est pas correcte.")
      */
-    private $email;
+    private string $email;
 
     public function getId(): ?int
     {
--- application/src/Form/UserType.php
+++ application/src/Form/UserType.php
@@ -17,15 +17,23 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username', TextType::class, ['label' => "Nom d'utilisateur"])
+            ->add('username', TextType::class, [
+                'label' => "Nom d'utilisateur"
+            ])
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Les deux mots de passe doivent correspondre.',
                 'required' => true,
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
+                'first_options' => [
+                    'label' => 'Mot de passe'
+                ],
+                'second_options' => [
+                    'label' => 'Tapez le mot de passe à nouveau'
+                ],
             ])
-            ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email'
+                ])
             ->add('roles', ChoiceType::class, [
                 'label' => 'Rôle',
                 'required' => true,
@@ -47,7 +55,7 @@ class UserType extends AbstractType
                 function ($rolesString) {
                     return [$rolesString];
                 }
-            ))
-        ;
+            )
+        );
     }
 }
